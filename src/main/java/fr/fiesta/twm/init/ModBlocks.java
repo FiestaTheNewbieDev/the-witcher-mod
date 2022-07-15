8675ac69f194270f9fc925fad53587423edab508
Clément FOSSORIER <clementfiesta@gmail.com>
2022-07-15 14:25:37 +0200

    version 2

--- src/main/java/fr/fiesta/twm/init/ModBlocks.java
+++ src/main/java/fr/fiesta/twm/init/ModBlocks.java
@@ -17,6 +17,9 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TWM.MODID);
 
     public static final RegistryObject<Block> SILVER_ORE = createBlock("silver_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(3f, 3f).harvestTool(ToolType.PICKAXE).harvestLevel(2).func_235861_h_()));
+
+    public static final RegistryObject<Block> METEORITE_ORE = createBlock("meteorite_ore", ()-> new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(3f, 3f).harvestTool(ToolType.PICKAXE).harvestLevel(2).func_235861_h_()));
+
     public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier) {
         RegistryObject<Block> block = BLOCKS.register(name, supplier);
         ModItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties()));
--- src/main/java/fr/fiesta/twm/init/ModItems.java
+++ src/main/java/fr/fiesta/twm/init/ModItems.java
@@ -16,9 +16,16 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_SILVER = ITEMS.register("raw_silver", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
     public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
 
+    public static final RegistryObject<Item> RAW_METEORITE = ITEMS.register("raw_meteorite", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
+    public static final RegistryObject<Item> METEORITE_INGOT = ITEMS.register("meteorite_ingot", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
+
+    public static final RegistryObject<Item> METEORITE_SILVER_INGOT = ITEMS.register("meteorite_silver_ingot", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
+
     public static final RegistryObject<Item> HANDLE = ITEMS.register("handle", ()-> new Item(new Item.Properties().group(ModItemGroups.TWM_TAB)));
 
     public static final RegistryObject<Item> SILVER_SWORD = ITEMS.register("silver_sword", ()-> new SwordItem(CustomItemTiers.SILVER, 3, -2.4F, new Item.Properties().maxStackSize(1).group(ModItemGroups.TWM_TAB)));
 
     public static final RegistryObject<BlockItem> SILVER_ORE = ITEMS.register("silver_ore_item", ()-> new BlockItem(ModBlocks.SILVER_ORE.get(), new Item.Properties().group(ModItemGroups.TWM_TAB)));
+
+    public static final RegistryObject<BlockItem> METEORITE_ORE = ITEMS.register("meteorite_ore_item", ()-> new BlockItem(ModBlocks.METEORITE_ORE.get(), new Item.Properties().group(ModItemGroups.TWM_TAB)));
 }
